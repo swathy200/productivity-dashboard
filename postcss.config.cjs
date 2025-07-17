d4b0f1ec12fb785f7cef7a165c74bff4b9a1f382
Swathy <swathyas2001@gmail.com>
2025-07-17 21:50:15 +0530

    initial commit

---
+++ postcss.config.cjs
@@ -0,0 +1,8 @@
+// postcss.config.cjs
+
+module.exports = {
+  plugins: [
+    require('@tailwindcss/postcss'),
+    require('autoprefixer')
+  ]
+}
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-green-500">
+      Hello Swathy! 🎉 Tailwind is working!
+    </div>
+  );
+}
+
+
+export default App
